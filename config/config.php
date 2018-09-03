ad84e50108d430b0f79cd592058629de9174eeed
outkastm <irinel2k@yahoo.com>
2018-09-03 21:52:49 +0200

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+//Default password is "root" with md5 hash
+//No "/" at the end of outputFolder
+
+return array(
+	"security" => true,
+	"password" => "63a9f0ea7bb98050796b649e85481845",
+	"outputFolder" => "/raid/data/downloads/YouTube",
+	"extracter" => "ffmpeg",
+	"max_dl" => 3);
+
+?>
